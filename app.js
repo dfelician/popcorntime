26a3d204cce86545cfb626ffd1628f4525ddf3e3
David <dfelician.97@gmail.com>
2022-08-01 01:45:58 -0400

    mongoDb connection

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const static = express.static(__dirname + '/public');
 const configRoutes = require('./routes');
 const exphbs = require('express-handlebars');
 
+//mongodb
+const connection = require('./config/mongoConnection');
+//TODO: add movies collection
+
+const main = async () => {
+const db = await connection.dbConnection();
+console.log("Connected to database");
+
 app.use('/public', static);
 app.use(express.json());
 app.use(express.urlencoded({
@@ -22,3 +30,6 @@ app.listen(3000, () => {
     console.log("We've now got a server!");
     console.log('Your routes will be running on http://localhost:3000');
 });
+};
+
+main();
